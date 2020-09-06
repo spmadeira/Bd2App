fa04018c9da7d29c0f367b09274e7fbabb25e9cd
Samuel <spmadeira@edu.unifor.br>
2020-09-06 01:22:30 -0300

    Initial Commit

---
+++ Bd2App/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+
+namespace Bd2App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public List<List<BucketRepresentation>> bucketRepresentations;
+        public int currentIndex;
+        
+        private readonly BackgroundWorker _worker = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _worker.DoWork += WorkerOnDoWork;
+            _worker.RunWorkerCompleted += WorkerOnRunWorkerCompleted;
+        }
+
+        private void WorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            DataGrid.ItemsSource = bucketRepresentations[currentIndex];
+            DataGrid.Visibility = Visibility.Visible;
+            LoadingIcon.Visibility = Visibility.Hidden;
+            PageControls.Visibility = Visibility.Visible;
+            PageLabel.Content = $"{currentIndex+1}/{bucketRepresentations.Count}";
+        }
+
+        private void WorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            dynamic arg = e.Argument;
+            
+            var lines = File.ReadAllLines((string)arg.Path);
+            var storage = new HashStorage<string, string>((int)arg.Pages, false);
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                storage.Store(line, line);
+            }
+            
+            bucketRepresentations = new List<List<BucketRepresentation>>();
+            
+            for (int i = 0; i < storage.Array.Length; i++)
+            {
+                var reps = new List<BucketRepresentation>();
+            
+                var bucket = storage.Array[i];
+                var counter = 0;
+                var entry = bucket.First;
+
+                while (entry != null)
+                {
+                    reps.Add(new BucketRepresentation
+                    {
+                        Bucket = i,
+                        HashValue = (int)Hasher.Hash(entry.Key),
+                        BucketIndex = counter++,
+                        Word = entry.Value
+                    });
+                    entry = entry.Next;
+                }
+            
+                bucketRepresentations.Add(reps);
+            }
+            
+            currentIndex = 0;
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void PageCounter_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out var _);
+        }
+
+        private void PageCounter_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                var text = (string)e.DataObject.GetData(typeof(string));
+                if (!int.TryParse(text, out var _)) e.CancelCommand();
+            }
+            else e.CancelCommand();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void LoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(PageCounter.Text, out var pageCount))
+                DialogHost.Show("Invalid Page Count");
+            
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                LoadingIcon.Visibility = Visibility.Visible;
+                _worker.RunWorkerAsync(new
+                {
+                    Path = ofd.FileName,
+                    Pages = pageCount
+                });
+            }
+        }
+
+        private void PrevPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                if (currentIndex == 0)
+                    PrevPageButton.IsEnabled = false;
+                if (!NextPageButton.IsEnabled)
+                    NextPageButton.IsEnabled = true;
+                DataGrid.ItemsSource = bucketRepresentations[currentIndex];
+                PageLabel.Content = $"{currentIndex+1}/{bucketRepresentations.Count}";
+            }
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentIndex < bucketRepresentations.Count)
+            {
+                currentIndex++;
+                if (currentIndex == bucketRepresentations.Count-1)
+                    NextPageButton.IsEnabled = false;
+                if (!PrevPageButton.IsEnabled)
+                    PrevPageButton.IsEnabled = true;
+                DataGrid.ItemsSource = bucketRepresentations[currentIndex];
+                PageLabel.Content = $"{currentIndex+1}/{bucketRepresentations.Count}";
+            }
+        }
+
+        public class BucketRepresentation
+        {
+            public int HashValue { get; set; }
+            public int Bucket { get; set; }
+            public int BucketIndex { get; set; }
+            public string Word { get; set; } 
+        }
+    }
+}
---
+++ Bd2App/Model/HashStorage.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//TODO: Refazer parte, ver como guardar um endereço que sempre é válido mesmo com os Rehashes
+//Dar uma olhada primeiro no pdf pra ver se não entra em conflito com nada
+
+public class HashStorage<TKey, TValue>
+{
+    public Bucket<TKey, TValue>[] Array { get; set; }
+    public bool CanRehash;
+    public int _values;
+
+    public HashStorage()
+    {
+        Array = new Bucket<TKey, TValue>[Hasher.GetPrime(0)];
+        CanRehash = true;
+    }
+
+    public HashStorage(int capacity, bool canRehash = true)
+    {
+        Array = new Bucket<TKey, TValue>[capacity];
+        CanRehash = canRehash;
+    }
+
+    public void Store(TKey key, TValue value)
+    {
+        _values++;
+        if (CanRehash && _values > Array.Length)
+        {
+            Rehash(Hasher.GetPrime(_values));
+        }
+
+        Insert(key, value);
+    }
+
+    public TValue Retrieve(TKey key)
+    {
+        var position = GetPosition(key);
+        var bucket = Array[position];
+        var entry = bucket.Get(key);
+        if (entry == null)
+            throw new ArgumentException("Key not found in storage");
+        return entry.Value;
+    }
+    
+    private void Insert(TKey key, TValue value)
+    {
+        var position = GetPosition(key);
+        Array[position].Push(key, value);
+    }
+    
+    private void Rehash(int newSize)
+    {
+        var values = Entries();
+        Array = new Bucket<TKey, TValue>[newSize];
+        foreach (var entry in values)
+        {
+            Insert(entry.Key, entry.Value);
+        }
+    }
+
+    private int GetPosition(TKey key)
+    {
+        var hash = Hasher.Hash(key.ToString());
+        var index = hash % Array.Length;
+        return (int) index;
+    }
+
+    public IEnumerable<BucketEntry<TKey, TValue>> Entries()
+    {
+        return Array.SelectMany(b => b.All());
+    }
+}
+
+public struct Bucket<TKey, TValue>
+{
+    public BucketEntry<TKey, TValue> First { get; set; }
+
+    public void Push(TKey key, TValue value)
+    {
+        if (First == null)
+        {
+            First = new BucketEntry<TKey, TValue>(key, value);
+            return;
+        }
+        else
+        {
+            var entry = First;
+
+            while (true)
+            {
+                if (entry.Key.Equals(key))
+                    throw new ArgumentException($"Key {key} already exists on storage");
+                if (entry.Next == null) break;
+                else entry = entry.Next;
+            }
+            
+            entry.Append(key, value);
+        }
+    }
+
+    public BucketEntry<TKey, TValue> Get(int position)
+    {
+        var entry = First;
+        while (position-- > 0)
+            entry = entry.Next;
+
+        return entry;
+    }
+    
+    public BucketEntry<TKey, TValue> Get(TKey key)
+    {
+        var entry = First;
+        if (entry == null) return null;
+
+        while (entry != null)
+        {
+            if (entry.Key.Equals(key))
+                return entry;
+            entry = entry.Next;
+        }
+
+        return null;
+    }
+
+    public int Length()
+    {
+        var length = 0;
+        var entry = First;
+        while (entry != null)
+        {
+            length++;
+            entry = entry.Next;
+        }
+
+        return length;
+    }
+
+    public IEnumerable<BucketEntry<TKey, TValue>> All()
+    {
+        if (First == null) return new BucketEntry<TKey, TValue>[0];
+
+        var list = new List<BucketEntry<TKey, TValue>>();
+        var entry = First;
+        while (entry != null)
+        {
+            list.Add(entry);
+            entry = entry.Next;
+        }
+
+        return list.ToArray();
+    }
+}
+
+public class BucketEntry<TKey, TValue>
+{
+    public BucketEntry<TKey, TValue> Next { get; set; }
+    public TKey Key { get; }
+    public TValue Value { get; }
+
+    public BucketEntry(TKey key, TValue value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public void Append(TKey key, TValue value)
+    {
+        Next = new BucketEntry<TKey, TValue>(key,value);
+    }
+}
